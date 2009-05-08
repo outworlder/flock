c364a43aad845a3aaa573f8438eed3bbc9495be7
Stephen Pedrosa Eilert <spedrosa@gmail.com>
2009-05-08 00:07:44 -0300

    - Even more brainstorming. - Added more code to process requests.

--- main.scm
+++ main.scm
@@ -3,28 +3,29 @@
 (require-extension uri-common)
 (require-extension intarweb)
 (require-extension sql-de-lite)
+(require-extension defstruct)
 
 ;; Steps:
 ;; - Get the request
 ;; - Break down into the various parts (host, port, etc)
+;;   - Update: There is also an URL decoding step, which is handled by the webserver itself.
 ;; - Go through the routes file trying to find a match
+;;   - Update: Since we are all trendy and weby-two-point-zeroy-like, the routes file identifies resources.
 ;; - Execute the matching action
 ;; - If no action matches, display the error page
 ;; - If the top exception handler catches an exception, display the 500 error page
 ;; - There are no controllers. When are we going to display the 404 page?
-
+;;   - Update: When a resource does not match.
 
 ;; TODO: Implement the model - requires SqLite access
-;; TODO: Figure out how to handle the controllers in a non-OO language. No OO frameworks will be added.
+;;   - Update: This will not be (at first) an Active-Record-like implementation. Instead, a plain old db access returning assoc-lists.
 
 ;; TODO: Add the exception handler here.
 (define (handle-errors)
   "Error page here blahblah")
 
-
 ;; Note about CGI requests:
 ;; CGI will send information using environment variables, command-line parameters and stdin
-
 
 ;; -----------------------------------------------------------------------------
 ;; Routes section
@@ -33,11 +34,34 @@
 ;; Routes will follow a simple structure at first.
 
 ;; URL breakdown:
-;; protocol://localhost.localdomain:port/controller/action/[named_param_1]/[named_param_2]?param3=value&param4=value
+;; protocol://localhost.localdomain:port/resource/.../resource/identifier&other_params
+;; TODO: Resources can be nested
+;; TODO[1]: If resources are nested, then we actually have a website tree, instead of an alist. Therefore, the route matcher will do a tree search instead.
+;; NOTE: Resources are essentially CRUDs, but they might choose not to implement a given operation.
+;; TODO[2]: Figure out how to describe nested resources. Nested s-exps are a possibility, but it can get messy.
 ;; Protocol and host are irrelevant to the route matcher
-;; 
-(define routes-alist
+;;
+(define routes-tree
   '())
+
+(define cgi-environment-variables-list
+   '("SERVER_SOFTWARE"
+  "SERVER_NAME"
+ "GATEWAY_INTERFACE"
+ "SERVER_PROTOCOL"
+ "SERVER_PORT"
+ "REQUEST_METHOD"
+ "PATH_INFO"
+ "PATH_TRANSLATED"
+ "SCRIPT_NAME"
+ "QUERY_STRING"
+ "REMOTE_HOST"
+ "REMOTE_ADDR"
+ "AUTH_TYPE"
+ "REMOTE_USER"
+ "REMOTE_IDENT"
+ "CONTENT_TYPE"
+ "CONTENT_LENGTH"))
 
 ;; Request method should not matter.
 ;; Process requests will initially process CGI requests. We have to parse the headers.
@@ -51,16 +75,20 @@
   (let ((request-params (decode-params)))
     ()))
 
-(define-syntax let-environment-variables
-  (syntax-rules ()
-    ((let-environment-variables ((name environment) ... ) forms ...)
-     (let ((name (get-environment-variable environment)) ...)
-       forms ...))))
+;; NOTE: Cute macro below was commented out. Turns out a plain alist is more than adequate. Thanks Freud.
+;; (define-syntax let-environment-variables
+;;   (syntax-rules ()
+;;     ((let-environment-variables ((name environment) ... ) forms ...)
+;;      (let ((name (get-environment-variable environment)) ...)
+;;        forms ...))))
 
 ;; This translates to:
 ;;(let ((server-software (get-environment-variable "SERVER_SOFTWARE")))
   ;; thunk
 ;;  )
+
+(define (get-env env-vars var)
+  (cadr (assoc var env-vars)))
 
 ;; Breaks down the URL into useful components.
 ;; Should return:
@@ -71,29 +99,28 @@
   (let ((uri-ref (uri-reference url)))
     (unless? (uri? uri-ref)
 	     (begin
-	       (uri-query (uri-ref))
-	       
-  
+	       (uri-query (uri-ref))))))
+
+;; Returns an alist containing the CGI environment variables
+(define (make-cgi-env-alist)
+  (map
+       (lambda (x)
+	 (list x (get-environment-variable x)))  cgi-environment-variables-list))
+
+;; Buzzword compliance code. Returns the intended verb.
+(define (restful-verb request)
+  (case (string->symbol (get-env request "REQUEST_METHOD"))
+    ((GET) 'read)
+    ((POST) 'create)
+    ((PUT) 'update)
+    ((DELETE) 'delete)))
+
+;; This calls the route matcher to forward requests to, according to the path and request method
+;; (buzzword-compliance "This is required for Restful routes")
+(define (handle-requests request verb uri)
+  #f)
 
 ;; This will decode the (initially) CGI parameters
 (define (decode-params)
-  (let-environment-variables
-   ((server-software "SERVER_SOFTWARE")
-    (server-name  "SERVER_NAME")
-    (gateway-interface-version "GATEWAY_INTERFACE")
-    (server-protocol "SERVER_PROTOCOL")
-    (server-port "SERVER_PORT")
-    (request-method "REQUEST_METHOD")
-    (path-info "PATH_INFO")
-    (path-translated "PATH_TRANSLATED")
-    (script-name "SCRIPT_NAME")
-    (query-string "QUERY_STRING")
-    (remote-host "REMOTE_HOST")
-    (remote-addr "REMOTE_ADDR")
-    (auth-type "AUTH_TYPE")
-    (remote-user "REMOTE_USER")
-    (remote-ident "REMOTE_IDENT")
-    (content-type "CONTENT_TYPE")
-    (content-length "CONTENT_LENGTH"))
-   (case request-method
-     (("GET") handle-get))
+  (let ((request (make-cgi-env-alist)))
+    (handle-requests request (restful-verb (get-env request "REQUEST_METHOD") (break-url (get-env request "PATH_INFO"))))))
