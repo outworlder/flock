c4fdec55c9a4c7d5c8842453059310c0bf34b232
Stephen Pedrosa Eilert <spedrosa@gmail.com>
2009-07-20 21:59:36 -0300

    - Starting from scratch.

--- main.scm
+++ main.scm
@@ -1,48 +1,13 @@
 ;; Flock framework
 
-(require-extension uri-common)
-(require-extension intarweb)
-(require-extension sql-de-lite)
+(require-extension uri-dispatch)
 (require-extension defstruct)
 
-;; Steps:
-;; - Get the request
-;; - Break down into the various parts (host, port, etc)
-;;   - Update: There is also an URL decoding step, which is handled by the webserver itself.
-;; - Go through the routes file trying to find a match
-;;   - Update: Since we are all trendy and weby-two-point-zeroy-like, the routes file identifies resources.
-;; - Execute the matching action
-;; - If no action matches, display the error page
-;; - If the top exception handler catches an exception, display the 500 error page
-;; - There are no controllers. When are we going to display the 404 page?
-;;   - Update: When a resource does not match.
-
-;; TODO: Implement the model - requires SqLite access
-;;   - Update: This will not be (at first) an Active-Record-like implementation. Instead, a plain old db access returning assoc-lists.
-
-;; TODO: Add the exception handler here.
-(define (handle-errors)
-  "Error page here blahblah")
-
-;; Note about CGI requests:
-;; CGI will send information using environment variables, command-line parameters and stdin
-
-;; -----------------------------------------------------------------------------
-;; Routes section
-;; -----------------------------------------------------------------------------
-
-;; Routes will follow a simple structure at first.
-
-;; URL breakdown:
-;; protocol://localhost.localdomain:port/resource/.../resource/identifier&other_params
-;; TODO: Resources can be nested
-;; TODO[1]: If resources are nested, then we actually have a website tree, instead of an alist. Therefore, the route matcher will do a tree search instead.
-;; NOTE: Resources are essentially CRUDs, but they might choose not to implement a given operation.
-;; TODO[2]: Figure out how to describe nested resources. Nested s-exps are a possibility, but it can get messy.
-;; Protocol and host are irrelevant to the route matcher
-;;
 (define routes-tree
   '())
+
+(defstruct response type extra-headers body)
+(defstruct request query_string path_info)
 
 (define cgi-environment-variables-list
    '("SERVER_SOFTWARE"
@@ -63,43 +28,8 @@
  "CONTENT_TYPE"
  "CONTENT_LENGTH"))
 
-;; Request method should not matter.
-;; Process requests will initially process CGI requests. We have to parse the headers.
-;; Request processing pseudocode:
-;; 1- Get request and query parameters. Split it into request host, port, and query
-;; 2- Decode query URL
-;; 3- Match against the route rules
-;; 4- Split the data between route variables and params (as in Rails, get or post parameters)
-;; OPTIONAL: Restful routes: Interpret the request verb and redirect to the appropriate action.
-(define (process-requests)
-  (let ((request-params (decode-params)))
-    ()))
-
-;; NOTE: Cute macro below was commented out. Turns out a plain alist is more than adequate. Thanks Freud.
-;; (define-syntax let-environment-variables
-;;   (syntax-rules ()
-;;     ((let-environment-variables ((name environment) ... ) forms ...)
-;;      (let ((name (get-environment-variable environment)) ...)
-;;        forms ...))))
-
-;; This translates to:
-;;(let ((server-software (get-environment-variable "SERVER_SOFTWARE")))
-  ;; thunk
-;;  )
-
 (define (get-env env-vars var)
   (cadr (assoc var env-vars)))
-
-;; Breaks down the URL into useful components.
-;; Should return:
-;; Controller
-;; Action
-;; (List of parameters)
-(define (break-url url)
-  (let ((uri-ref (uri-reference url)))
-    (unless? (uri? uri-ref)
-	     (begin
-	       (uri-query (uri-ref))))))
 
 ;; Returns an alist containing the CGI environment variables
 (define (make-cgi-env-alist)
@@ -107,20 +37,50 @@
        (lambda (x)
 	 (list x (get-environment-variable x)))  cgi-environment-variables-list))
 
-;; Buzzword compliance code. Returns the intended verb.
-(define (restful-verb request)
-  (case (string->symbol (get-env request "REQUEST_METHOD"))
-    ((GET) 'read)
-    ((POST) 'create)
-    ((PUT) 'update)
-    ((DELETE) 'delete)))
+(define (main)
+  (print "Content-type: text/html\n\n")
+  (print "Hello world from hell!"))
 
-;; This calls the route matcher to forward requests to, according to the path and request method
-;; (buzzword-compliance "This is required for Restful routes")
-(define (handle-requests request verb uri)
-  #f)
+(define-view 'index
+  (html
+   (head
+    (title "Stone-age blog")
+    (stylesheet "stone-age.css"))
+   (body
+    (render-blog-post))))
 
-;; This will decode the (initially) CGI parameters
-(define (decode-params)
-  (let ((request (make-cgi-env-alist)))
-    (handle-requests request (restful-verb (get-env request "REQUEST_METHOD") (break-url (get-env request "PATH_INFO"))))))
+(define (render-view name)
+  ())
+
+(define (handle-errors message)
+  (print-content-type 'html)
+  (print message))
+
+(define (decide-content-type type)
+  (case type
+    ((html) "text/html")
+    ((js) "application/javascript")
+    ((css) "text/stylesheet")
+    ((text) "text/plain")
+    ((gif) "image/gif")
+    ((jpeg) "image/jpeg")
+    ((png) "image/png")
+    ((xml) "text/xml")
+    (else "application/octet-stream"))) ;TODO: Follow the RFC (http://www.w3.org/Protocols/rfc2616/rfc2616-sec7.html#sec7.2.1) to the letter and figure out what to do with unknown types
+
+(define (print-content-type type)
+  (print "Content-type: " (decide-content-type type))
+  (print))
+   
+;; Headers is an association list, with the headers that are going into the response. If nil(or empty),
+;; render a standard set of headers
+(define (render-response response)
+  (unless (response? response)
+    (handle-errors "render-response: Asked to render something that's not a response object.")
+    (print-content-type (response-type response))
+    (display ((response-body response)))))
+
+(define (index)
+  (render-view 'index))
+
+(main)
