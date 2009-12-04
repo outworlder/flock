fd57208ff202bbca147408f8f3be12fc4a09d471
Stephen Eilert <stephen@atlantico2354.(none)>
2009-12-04 18:24:11 -0300

    - Fixed the other blog pages

--- src/comment_posted.scm
+++ src/comment_posted.scm
@@ -1,8 +1,11 @@
-(define (comment-posted-view)
-  `((div (@ (class "header"))
-         (h1 "Post a new comment")
-         (h2 "Comment posted successfully..."))
-    (h1 "Result" (div ,(handle-post)))))
+(define (comment-posted #!rest args)
+  (define-page "Comment posted"
+    (stylesheet-link "/paleolithic.css")
+    (lambda ()
+      `((div (@ (class "header"))
+             (h1 "Post a new comment")
+             (h2 "Comment posted successfully..."))
+        (h1 "Result" (div ,(handle-post)))))))
 
 ;; (send-cgi-response (html-body "Comment posted" comment-posted-view
 ;;                               (stylesheet-link "/paleolithic.css")))
--- src/post.scm
+++ src/post.scm
@@ -10,12 +10,15 @@
             (input (@ (type submit) (value "Submit")))
             (input (@ (type reset) (value "Clear")))))))
 
-(define (post-view)
-  `((div (@ (class "header"))
-         (h1 "New blog post")
-         (h2 "Lambda"))
-    (br)
-    ,(make-form-post)))
+(define (post #!rest args)
+  (define-page "Post a new comment"
+    (stylesheet-link "/paleolithic.css")
+    (lambda ()
+      `((div (@ (class "header"))
+             (h1 "New blog post")
+             (h2 "Lambda"))
+        (br)
+        ,(make-form-post)))))
 
 ;; (send-cgi-response
 ;;  (html-body "Post a new comment" post-view
