7aa4c8561bfe3108ee6e59ead359a9fa4e1a6867
Stephen Eilert <spedrosa@Polaris.local>
2010-05-24 15:43:50 -0300

    - Form layout changes.

--- src/comment.scm
+++ src/comment.scm
@@ -1,6 +1,10 @@
 (use awful)
 (use html-utils)
 
+;; (define-page "/comment_posted"
+;;   (let 
+
+
 ;; TODO: Need to come up with a solution for XSS vulnerabilities.
 (define (render-comment post-id)
   (let ([comments (get-comments-by-post-id post-id)])
@@ -22,20 +26,26 @@
   (++
    (form 
     (<div> class: "comment_form"
-           (<fieldset>
-            (<legend> "Post a comment")
+           (<legend> "Post a comment")
+           (<hr>)
             (<span> class: "author_box"
-                    (<label> for: "author_name" "Name: ")
-                    (text-input "Name:" id: "author_name"))
+                    (<p>
+                     (<label> for: "author_name" "Name ")
+                     (text-input "Name" id: "author_name")))
             (<span> class: "email_box"
-                    (<label> for: "author_email" "Email: ")
-                    (text-input "Email:" id: "author_email"))
+                    (<p>
+                     (<label> for: "author_email" "Email ")
+                     (text-input "Email" id: "author_email")))
+            (<span> class: "url_box"
+                    (<p>
+                     (<label> for: "author_url" "Url ")
+                     (text-input "Url" id: "author_url")))
             (<span> class: "Comment"
                     (<textarea> id: "comment_area"))
             (<span> class: "buttons"
-                    (submit-input value: "Post comment")
-                    (<input> type: "reset" id: "reset_button")
-                    (<input> type: "hidden" id: "post-id" value: post-id))))
+                    (submit-input value: "Submit comment")
+                    ;; (<input> type: "reset" id: "reset_button")
+                    (<input> type: "hidden" id: "post-id" value: post-id)))
            action: "post_comment" method: 'post)))
 
                   
