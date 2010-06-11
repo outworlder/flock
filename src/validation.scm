a9161d02c9dfc80e438778449e694f6228c42568
Stephen Eilert <spedrosa@gmail.com>
2010-06-10 23:25:03 -0300

    - Adding some validation macros. Still not completely tested.

--- src/validation.scm
+++ src/validation.scm
@@ -1,5 +1,4 @@
 (use awful)
-(use regex-literals)
 (use srfi-1)
 
 ;; Fields is a alist of fields and the required validations.
@@ -14,22 +13,23 @@
 ;; TODO: Add forms to execute for when the validation passes and for when validation fails.
 (define-syntax with-validations
   (syntax-rules ()
-    ([_ ((field (validation params ...) ...) ...) ] (begin
-                                                      (let ([validation-results
-                                                             (list
-                                                              (let ([field-value ($ 'field #f)])
-                                                                (++
-                                                                 (let ([validation-result 
-                                                                        (validation field-value params ...)])
-                                                                   (if (string? validation-result) ;Validation failed
-                                                                       validation-result)) ...)) ...)])
-                                                        (if (> (length (filter string? validation-results)) 0)
-                                                            validation-results ;Validation failed
-                                                            #t))))))
+    ([_ validation-ok validation-fail ((field (validation params ...) ...) ...) ]
+     (begin
+       (let ([validation-results
+              (list
+               (let ([field-value ($ 'field #f)])
+                 (++
+                  (let ([validation-result 
+                         (validation field-value params ...)])
+                    (if (string? validation-result) ;Validation failed
+                        validation-result)) ...)) ...)])
+         (if (> (length (filter string? validation-results)) 0)
+             (validation-fail validation-results) ;Validation failed
+             (validation-ok)))))))
 
  
 ;; Each validation function takes exactly one param
-(define (validate-presence value #!key (message "teste"))
+(define (validate-presence value #!key (message " is required"))
   (if (string? value)
       #t
       message))
@@ -43,19 +43,25 @@
         (assoc key flash-data)
         flash-data)))
 
-
 ;; Example:
 
 ;; (with-validations
-;;  (age (validates-presence)
-;;       (validates-numericality))
-;;  (email (validates-presence)
-;;         (validates-length maximum: 20)
-;;         (validates-format "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}" message: "Email address should be in the format someone@domain.com")))
-
+;;  (lambda ()
+;;    "OK!")
+;;  (lambda (results)
+;;    (string-append "NOT OK: " results))
+;;  ((age (validates-presence))))
+;;        (validates-numericality))
+;;   (email (validates-presence)
+;;          (validates-length maximum: 20)
+;;          (validates-format "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}" message: "Email address should be in the format someone@domain.com"))))
+;;                                         ;successful
+;;                                         ;not successful
+;;  )
+  
 
 (define (validates-presence field #!key if message)
-  #f)
+  "")
 
 (define (validates-confirmation field1 field2)
   #f)
